4e590ff61abe5d3edc5e2231457e63ce36f6fdf4
Salaboy <salaboy@gmail.com>
2011-02-14 19:23:44 -0300

    adding dataInput parameters to the sample process

--- 01-jBPM5-SimpleEmergencyServiceProcess/src/main/java/com/wordpress/salaboy/example/model/Emergency.java
+++ 01-jBPM5-SimpleEmergencyServiceProcess/src/main/java/com/wordpress/salaboy/example/model/Emergency.java
@@ -8,4 +8,21 @@ package com.wordpress.salaboy.example.model;
  * To change this template use File | Settings | File Templates.
  */
 public class Emergency {
+    private int revision = 0 ;
+    private String phoneCall;
+    public Emergency(String phoneCall) {
+        this.phoneCall = phoneCall;
+    }
+
+    public String getPhoneCall() {
+        return phoneCall;
+    }
+
+    public void setRevision(int revision) {
+        this.revision = revision;
+    }
+
+    public int getRevision() {
+        return revision;
+    }
 }
--- 01-jBPM5-SimpleEmergencyServiceProcess/src/test/java/com/wordpress/salaboy/examples/SimpleEmergencyProcessTest.java
+++ 01-jBPM5-SimpleEmergencyServiceProcess/src/test/java/com/wordpress/salaboy/examples/SimpleEmergencyProcessTest.java
@@ -8,7 +8,6 @@ import org.drools.io.impl.ClassPathResource;
 import org.drools.logger.KnowledgeRuntimeLoggerFactory;
 import org.drools.runtime.StatefulKnowledgeSession;
 import org.drools.runtime.process.*;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -40,13 +39,13 @@ public class SimpleEmergencyProcessTest {
     public void emergencyServiceAutomaticHumanTasksTest() throws InterruptedException {
         KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
         kbuilder.add(new ClassPathResource("EmergencyServiceSimple.bpmn"), ResourceType.BPMN2);
-        KnowledgeBuilderErrors errors= kbuilder.getErrors();
-        if(errors.size() > 0){
-           for(KnowledgeBuilderError error : errors){
-             System.out.println(error.getMessage());
+        KnowledgeBuilderErrors errors = kbuilder.getErrors();
+        if (errors.size() > 0) {
+            for (KnowledgeBuilderError error : errors) {
+                System.out.println(error.getMessage());
 
-           }
-           return;
+            }
+            return;
         }
 
         KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
@@ -55,14 +54,14 @@ public class SimpleEmergencyProcessTest {
 
         ksession = kbase.newStatefulKnowledgeSession();
         MyHumanActivityAutomaticSimulatorWorkItemHandler humanActivitiesSimHandler = new MyHumanActivityAutomaticSimulatorWorkItemHandler();
-        ksession.getWorkItemManager().registerWorkItemHandler("Human Task", humanActivitiesSimHandler );
+        ksession.getWorkItemManager().registerWorkItemHandler("Human Task", humanActivitiesSimHandler);
 
         KnowledgeRuntimeLoggerFactory.newConsoleLogger(ksession);
 
         //Setting the process engine and the rule engine in reactive mode
         // This will cause that if a rule is activated, the rule will fire without waiting
         // the user to call the fireAllRules() method.
-        new Thread(new Runnable()       {
+        new Thread(new Runnable() {
 
             public void run() {
                 ksession.fireUntilHalt();
@@ -74,8 +73,6 @@ public class SimpleEmergencyProcessTest {
         Thread.sleep(1000);
 
         Assert.assertEquals(ProcessInstance.STATE_COMPLETED, process.getState());
-
-
 
 
     }
@@ -92,13 +89,13 @@ public class SimpleEmergencyProcessTest {
     public void emergencyServiceNonAutomaticHumanTasksTest() throws InterruptedException {
         KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
         kbuilder.add(new ClassPathResource("EmergencyServiceSimple.bpmn"), ResourceType.BPMN2);
-        KnowledgeBuilderErrors errors= kbuilder.getErrors();
-        if(errors.size() > 0){
-           for(KnowledgeBuilderError error : errors){
-             System.out.println(error.getMessage());
+        KnowledgeBuilderErrors errors = kbuilder.getErrors();
+        if (errors.size() > 0) {
+            for (KnowledgeBuilderError error : errors) {
+                System.out.println(error.getMessage());
 
-           }
-           return;
+            }
+            return;
         }
 
         KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
@@ -107,14 +104,14 @@ public class SimpleEmergencyProcessTest {
 
         ksession = kbase.newStatefulKnowledgeSession();
         MyHumanActivityNonAutomaticSimulatorWorkItemHandler humanActivitiesSimHandler = new MyHumanActivityNonAutomaticSimulatorWorkItemHandler();
-        ksession.getWorkItemManager().registerWorkItemHandler("Human Task", humanActivitiesSimHandler );
+        ksession.getWorkItemManager().registerWorkItemHandler("Human Task", humanActivitiesSimHandler);
 
         KnowledgeRuntimeLoggerFactory.newConsoleLogger(ksession);
 
         //Setting the process engine and the rule engine in reactive mode
         // This will cause that if a rule is activated, the rule will fire without waiting
         // the user to call the fireAllRules() method.
-        new Thread(new Runnable()       {
+        new Thread(new Runnable() {
 
             public void run() {
                 ksession.fireUntilHalt();
@@ -136,7 +133,7 @@ public class SimpleEmergencyProcessTest {
         Thread.sleep(1000);
         System.out.println("Completing the second Activity");
         //Complete the second human activity
-         Assert.assertEquals(ProcessInstance.STATE_ACTIVE, process.getState());
+        Assert.assertEquals(ProcessInstance.STATE_ACTIVE, process.getState());
         Thread.sleep(1000);
         Assert.assertEquals(1, process.getNodeInstances().size());
         Assert.assertEquals("Dispatch Ambulance", process.getNodeInstances().iterator().next().getNodeName());
@@ -151,88 +148,137 @@ public class SimpleEmergencyProcessTest {
     @Test
     public void emergencyServiceWithInputDataTest() throws InterruptedException {
         KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
-                kbuilder.add(new ClassPathResource("EmergencyServiceSimple.bpmn"), ResourceType.BPMN2);
-                KnowledgeBuilderErrors errors= kbuilder.getErrors();
-                if(errors.size() > 0){
-                   for(KnowledgeBuilderError error : errors){
-                     System.out.println(error.getMessage());
+        kbuilder.add(new ClassPathResource("EmergencyServiceSimple.bpmn"), ResourceType.BPMN2);
+        KnowledgeBuilderErrors errors = kbuilder.getErrors();
+        if (errors.size() > 0) {
+            for (KnowledgeBuilderError error : errors) {
+                System.out.println(error.getMessage());
 
-                   }
-                   return;
-                }
+            }
+            return;
+        }
 
-                KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
-                kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
+        KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
+        kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
 
 
-                ksession = kbase.newStatefulKnowledgeSession();
-                MyHumanActivityAutomaticSimulatorWorkItemHandler humanActivitiesSimHandler = new MyHumanActivityAutomaticSimulatorWorkItemHandler();
-                ksession.getWorkItemManager().registerWorkItemHandler("Human Task", humanActivitiesSimHandler );
+        ksession = kbase.newStatefulKnowledgeSession();
+        MyHumanActivitySimulatorChangeValuesWorkItemHandler humanActivitiesSimHandler = new MyHumanActivitySimulatorChangeValuesWorkItemHandler();
+        ksession.getWorkItemManager().registerWorkItemHandler("Human Task", humanActivitiesSimHandler);
 
-                KnowledgeRuntimeLoggerFactory.newConsoleLogger(ksession);
+        KnowledgeRuntimeLoggerFactory.newConsoleLogger(ksession);
 
-         //Setting the process engine and the rule engine in reactive mode
+        //Setting the process engine and the rule engine in reactive mode
         // This will cause that if a rule is activated, the rule will fire without waiting
         // the user to call the fireAllRules() method.
-        new Thread(new Runnable()       {
+        new Thread(new Runnable() {
 
             public void run() {
                 ksession.fireUntilHalt();
             }
         }).start();
 
-            Map<String, Object> parameters = new HashMap<String, Object>();
-            parameters.put("emergency", new Emergency());
+        Map<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("emergency", new Emergency("555-1234"));
 
 
-            WorkflowProcessInstance process = (WorkflowProcessInstance) ksession.startProcess("com.wordpress.salaboy.bpmn2.SimpleEmergencyService", parameters );
+        WorkflowProcessInstance process = (WorkflowProcessInstance) ksession.startProcess("com.wordpress.salaboy.bpmn2.SimpleEmergencyService", parameters);
 
-         Thread.sleep(1000);
+        Assert.assertEquals(ProcessInstance.STATE_ACTIVE, process.getState());
 
-        Assert.assertEquals(ProcessInstance.STATE_COMPLETED, process.getState());
+        Assert.assertEquals(1, process.getNodeInstances().size());
+        Assert.assertEquals("Ask for Emergency Information", process.getNodeInstances().iterator().next().getNodeName());
+        Assert.assertEquals(1, ((Emergency)process.getVariable("emergency")).getRevision());
+        System.out.println("Completing the first Activity");
+        //Complete the first human activity
+        humanActivitiesSimHandler.completeWorkItem();
+        Assert.assertEquals(ProcessInstance.STATE_ACTIVE, process.getState());
 
+        Thread.sleep(1000);
+        System.out.println("Completing the second Activity");
+        //Complete the second human activity
+        Assert.assertEquals(ProcessInstance.STATE_ACTIVE, process.getState());
+        Thread.sleep(1000);
+        Assert.assertEquals(1, process.getNodeInstances().size());
+        Assert.assertEquals("Dispatch Ambulance", process.getNodeInstances().iterator().next().getNodeName());
+        Assert.assertEquals(2, ((Emergency)process.getVariable("emergency")).getRevision());
+        humanActivitiesSimHandler.completeWorkItem();
+
+
+        Thread.sleep(1000);
     }
 
     @Test
-    public void  emergencyServiceWithRulesTest(){
+    public void emergencyServiceWithRulesTest() {
 
 
     }
 
 
+}
+
+class MyHumanActivityNonAutomaticSimulatorWorkItemHandler implements WorkItemHandler {
+    private WorkItemManager workItemManager;
+    private long workItemId;
+
+    public void executeWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
+        this.workItemId = workItem.getId();
+        this.workItemManager = workItemManager;
+        System.out.println("Map of Parameters = " + workItem.getParameters());
+    }
+
+    public void abortWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
+
+    }
+
+    public void completeWorkItem(Map<String, Object> parameters) {
+        this.workItemManager.completeWorkItem(this.workItemId, parameters);
+
+    }
+
+}
 
 
-     private class MyHumanActivityAutomaticSimulatorWorkItemHandler implements WorkItemHandler{
+class MyHumanActivitySimulatorChangeValuesWorkItemHandler implements WorkItemHandler {
+    private static int counter = 1;
+    private WorkItemManager workItemManager;
+    private long workItemId;
+    private Map<String, Object> results;
+    private Emergency currentEmergency;
+    public void executeWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
+         this.workItemId = workItem.getId();
+        this.workItemManager = workItemManager;
+        currentEmergency = (Emergency) workItem.getParameter("emergency");
+        currentEmergency.setRevision(currentEmergency.getRevision() + counter);
 
-            public void executeWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
-
-                   workItemManager.completeWorkItem(workItem.getId(), null);
-            }
-
-            public void abortWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
-
-            }
 
 
-        }
-    private class MyHumanActivityNonAutomaticSimulatorWorkItemHandler implements WorkItemHandler{
-            private WorkItemManager workItemManager;
-            private long workItemId;
+    }
 
-            public void executeWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
-                this.workItemId = workItem.getId();
-                this.workItemManager = workItemManager;
+    public void abortWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
 
-            }
+    }
 
-            public void abortWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
+    public void completeWorkItem() {
+        results = new HashMap<String, Object>();
+        results.put("emergency", currentEmergency);
+        workItemManager.completeWorkItem(workItemId, results);
 
-            }
+    }
 
-            public void completeWorkItem(Map<String, Object> parameters){
-                 this.workItemManager.completeWorkItem(this.workItemId, parameters);
 
-            }
+}
 
-        }
+class MyHumanActivityAutomaticSimulatorWorkItemHandler implements WorkItemHandler {
+
+    public void executeWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
+        System.out.println("Map of Parameters = " + workItem.getParameters());
+        workItemManager.completeWorkItem(workItem.getId(), null);
+    }
+
+    public void abortWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
+
+    }
+
+
 }
